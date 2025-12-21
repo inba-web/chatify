38296f2d223ac32ad46e1dfe81b8fbef29699412
Inba <inba.web@gmail.com>
2025-12-21 21:59:28 +0530

    initial commit

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/send", (req,res) =>{
+    res.send("Send Your Message");
+})
+
+
+
+export default router;
---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "../src/routes/auth.route.js";
+import messageRoutes from "../src/routes/message.route.js";
+
+
+
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT || 3000; 
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+
+app.listen(PORT,() => {
+    console.log("Server Running on Port 3000 ")
+})

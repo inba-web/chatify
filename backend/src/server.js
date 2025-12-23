afe3717c044a58910ad8b2cca038288462188b88
Inba <inba.web@gmail.com>
2025-12-24 05:04:27 +0530

    signup contoller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,29 +3,30 @@ import dotenv from "dotenv";
 import authRoutes from "../src/routes/auth.route.js";
 import messageRoutes from "../src/routes/message.route.js";
 import path from "path";
+import { connectDB } from "../src/lib/db.js";
 
-
-dotenv.config()
+dotenv.config();
 
 const app = express();
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000;
 
+app.use(express.json()) 
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-
 // make ready for deployment
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*",(_,res) => {
-        res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"))
-    })
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-app.listen(PORT,() => {
-    console.log("Server Running on Port 3000 ")
-})
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server Running on Port 3000 ");
+});

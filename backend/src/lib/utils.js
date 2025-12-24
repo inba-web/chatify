1350199d80c570b0396957b4df427351626f7ae5
Inba <inba.web@gmail.com>
2025-12-24 15:49:09 +0530

    login & logout function implementer

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken = (userId,res) => {
 
     res.cookie("jwt",token, {
         maxAge: 7*24*60*60*1000,
-        httpOnly: true, // prevent xss attack
+        httpOnly: true, // prevent xss attack 
         sameSite: "strict",  // prevent csrf attack
         secure: ENV.NODE_ENV === "development" ? false : true,
     });

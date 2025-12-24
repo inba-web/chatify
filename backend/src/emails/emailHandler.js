8a743a8f24cdfbf0ad9d0f68af6e5c0f7b1c4bec
Inba <inba.web@gmail.com>
2025-12-24 14:52:21 +0530

    email implementation

---
+++ backend/src/emails/emailHandler.js
@@ -0,0 +1,18 @@
+import { resendClient, sender } from "../lib/resend.js"
+import { createWelcomeEmailTemplate } from "../emails/emailTemplate.js";
+
+export const sendWelcomeEmail = async (email,name,clientURL) =>{
+    const {data,error} = await resendClient.emails.send({
+        from : `${sender.name} <${sender.email}>`,
+        to : email,
+        subject : "Welcome to Chatify! 🎉",
+        html : createWelcomeEmailTemplate(name,clientURL)
+    })
+
+    if(error){
+        console.error("Error in sending welcome Email (Resend API error): ", error);
+        throw new Error("Failed to send welcome email");
+    }
+
+    console.log("Welcome email sent successfully", data); 
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,13 +4,14 @@ import authRoutes from "../src/routes/auth.route.js";
 import messageRoutes from "../src/routes/message.route.js";
 import path from "path";
 import { connectDB } from "../src/lib/db.js";
+import {ENV} from "./lib/env.js";
 
 dotenv.config();
 
 const app = express();
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 3000;
+const PORT = ENV.PORT || 3000;
 
 app.use(express.json()) 
 
@@ -18,7 +19,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
-if (process.env.NODE_ENV === "production") {
+if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (_, res) => {

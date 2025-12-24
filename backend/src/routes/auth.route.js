bc63920c384489f935c70a28e833e8cd7a209817
Inba <inba.web@gmail.com>
2025-12-25 00:51:31 +0530

    rate limiting implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js ";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
+router.use(arcjetProtection);
 
 router.post("/signup", signup) 
 router.post("/login", login)
@@ -13,6 +15,6 @@ router.post("/update-profile", protectRoute , updateProfile)
 
 router.get("/check",protectRoute, (req, res) => {
     return res.status(200).json(req.user);
-})
+}) 
 
 export default router;

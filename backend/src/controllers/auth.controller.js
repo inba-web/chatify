59bb0005f3ed6f43046af70982c0fed010950352
Inba <inba.web@gmail.com>
2025-12-24 22:51:31 +0530

    update profile endpoint added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,6 +3,8 @@ import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
 import { sendWelcomeEmail } from "../emails/emailHandler.js";
 import { ENV } from "../lib/env.js";
+import cloudinary from "../lib/cloudinary.js";
+
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -28,14 +30,13 @@ export const signup = async (req, res) => {
       return res.status(400).json({ message: "Invalid Email Format" });
     }
 
-    const salt = await bcrypt.genSalt(10);
+    const salt = await bcrypt.genSalt(10); 
     const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = new User({
       fullName,
       email,
       password: hashedPassword,
     });
-
     await newUser.save();
 
     if (newUser) {
@@ -96,3 +97,23 @@ export const logout = async (_, res) => {
   res.cookie("jwt", "", { maxAge: 0 });
   res.status(200).json({ message: "Logged Out Successfully" });
 };
+
+export const updateProfile = async (req,res) => {
+  try {
+    const {profilePic} = req.body;
+    if(!profilePic) return res.status(400).json({message:"Profile pic is required"})
+
+    const userId = req.user._id;
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+    const updateUser = await User.findByIdAndUpdate(userId, {
+      profilePic:uploadResponse.secure_url 
+    }, {new:true})
+
+    res.status(200).json(updateUser)
+  } catch (error) {
+    console.error("Error in update profile", error);;
+    res.status(500).json({message:"Internal Server Error"})
+  }
+}
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
-
 const UserSchema = new mongoose.Schema({
     email:{
         type:String,

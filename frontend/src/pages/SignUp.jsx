4bf487595ebfad34f633d5f5c7cad2e73bfb21e1
Inba <inba.web@gmail.com>
2025-12-26 19:24:39 +0530

    signup implemented on the frontend

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,9 +1,125 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useAuthStore } from "../store/useAuthStore.js";
+import BorderAnimatedContainer from '../components/BorderAnimatedContainer.jsx';
+import {MailIcon, MessageCircleIcon, UserIcon, LockIcon} from "lucide-react";
+import { LoaderIcon } from 'react-hot-toast';
+import { Link } from "react-router";
+
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+  const [formData, setFormData] = useState({fullName:"",email:"",password:""}) 
+  const {signup, isSigningUp} = useAuthStore(); 
 
-export default SignUp
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signup(formData);
+  } 
+
+  return (
+    <div className='w-full flex items-center justify-center p-4 bg-slate-900'>
+      <div className='relative w-full max-w-6xl md:h-[800px] h-[650px]'>
+        {/* BORDER ANIMATED CONTAINER */}
+
+        <BorderAnimatedContainer> 
+
+          <div className='w-full flex flex-col md:flex-row'>
+
+            {/* FORM COLUMN LEFT SIDE */} 
+            <div className='md:w-1/2 p-8 flex items-center justify-center md:border-r border-slate-600/30'>
+              <div className='w-full max-w-md'>
+                {/* HEADING */}
+                <div className='text-center mb-8'>
+                  <MessageCircleIcon className="w-12 h-12 mx-auto test-slate-400 mb-4" />
+                  <h2 className='text-2xl font-bold text-slate-200 mb-2'>Create Account</h2>
+                  <p className='text-slate-400'>Sign up for a new account</p>
+                </div>
+
+                {/* FORM */}
+                <form onSubmit={handleSubmit} className='space-y-6'>
+                  <div>
+                    <label className='auth-input-label'>Full Name</label>
+                    <div className='relative'>
+                      <UserIcon className='auth-input-icon'/>
+                      <input type="text"
+                        value={formData.fullName}
+                        onChange={(e) => setFormData({...formData, fullName : e.target.value})} 
+                        className='input'
+                        placeholder='John Does'
+                      />
+                    </div>
+                  </div>
+
+                  {/* EMAIL INPUT */}
+                  <div>
+                    <label className='auth-input-label'>Email</label>
+                    <div className='relative'>
+                      <MailIcon className='auth-input-icon'/>
+                      <input type="email"
+                        value={formData.email}
+                        onChange={(e) => setFormData({...formData, email:e.target.value})}
+                        className='input'
+                        placeholder='Johndoe@gmail.com'
+                      />
+                    </div>
+                  </div>
+
+                  {/* PASSWORD INPUT */}
+                  <div>
+                    <label className='auth-input-label'>Password</label>
+                    <div className='relative'>
+                      <LockIcon className='auth-input-icon'/>
+                      <input type="password"
+                        value={formData.password}
+                        className='input'
+                        onChange={(e) => setFormData({...formData, password:e.target.value})}
+                        placeholder='Enter Your Password' 
+                      />
+                    </div>
+                  </div>
+
+                  {/* SUBMIT BUTTON */}
+
+                  <button className='auth-btn' type='submit' disabled={isSigningUp}>
+                    {isSigningUp ? (
+                      <LoaderIcon className='w-full h-5 animate-spin text-center'/>
+                    ) : (
+                      "Create Account"
+                    )
+
+                    }
+                  </button>
+                </form>
+                
+                <div className='mt-6 text-center'>
+                  <Link to="/login" className='auth-link'>
+                    Already have an account ? login
+                  </Link>
+                </div>
+
+              </div>
+            </div>
+            {/* FORM RIGHT SIDE */}
+          <div className='hidden md:w-1/2 md:flex items-center justify-center p-6 bg-gradient-to-bl from-slate-800/20 to-transparent'>
+            <div>
+              <img src="/signup.png" alt="People using mobile devices" className='w-full h-auto object-contain' /> 
+              <div className='mt-6 text-center'>
+                <h3 className='text-xl font-medium text-cyan-400'>Start Your Journey Today</h3>
+
+                <div className='mt-4 flex justify-center gap-4'>
+                  <span className='auth-badge'>Free</span>
+                  <span className='auth-badge'>Easy Setup</span>
+                  <span className='auth-badge'>Private</span> 
+                </div>
+
+              </div>
+            </div>
+          </div>
+          
+          </div>
+        </BorderAnimatedContainer>
+      </div>
+    </div>
+  ) 
+} 
+
+export default SignUp;

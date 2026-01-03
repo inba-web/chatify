3eda2c171b955f5fba5f62d76fa85b7c63eae1ed
Inba <inba.web@gmail.com>
2026-01-03 19:19:51 +0530

    feat : keyboard keystroke sounds button bug fixed

--- frontend/src/hooks/useKeyboardSound.js
+++ frontend/src/hooks/useKeyboardSound.js
@@ -1,9 +1,9 @@
 
 const keyStrokeSounds = [
-  new Audio("/sounds/keystroke1.mps"),
-  new Audio("/sounds/keystroke2.mps"),
-  new Audio("/sounds/keystroke3.mps"),
-  new Audio("/sounds/keystroke4.mps"),
+  new Audio("/sounds/keystroke1.mp3"),
+  new Audio("/sounds/keystroke2.mp3"),
+  new Audio("/sounds/keystroke3.mp3"),
+  new Audio("/sounds/keystroke4.mp3"),
 ];
 
 function useKeyboardSound() {

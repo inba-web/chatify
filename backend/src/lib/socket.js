feb63725b6a5c5c50d0a3f18845bf4f995c3dbf6
Inba <inba.web@gmail.com>
2026-01-06 20:48:15 +0530

    fixed the bugs and ready for deplyment

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [ENV.CLIENT_URL],
+    origin: [ENV.CLIENT_URL, "http://localhost:3000"],
     credentials: true,
   },
 });
